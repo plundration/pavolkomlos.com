655a977cf0562ff1f488479249d5bda1c2670196
plundration <matouinhell@gmail.com>
2022-11-07 22:00:46 +0100

    Work on website looks and content

--- tagcloud.js
+++ tagcloud.js
@@ -15,21 +15,6 @@ const myTags = [
     "RayLib",
 ];
 
-console.log(document.querySelector(".content"));
-var tagCloud = TagCloud(".content", myTags, {
-    // radius in px
-    radius: 200,
+var tagCloud = TagCloud(".big-cloud", myTags, { radius: 220});
+var tagCloud = TagCloud(".small-cloud", myTags, { radius: 140});
 
-    // animation speed
-    // slow, normal, fast
-    maxSpeed: "fast",
-    initSpeed: "fast",
-
-    // 0 = top
-    // 90 = left
-    // 135 = right-bottom
-    direction: 135,
-
-    // interact with cursor move on mouse out
-    keep: true,
-});
